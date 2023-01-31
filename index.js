6723d70b46bab0cd1792f2109c46bc64dd2bb599
Christopher Wong <cw30355@gmail.com>
2023-01-31 14:47:59 -0500

    added npm tests

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name){
+    const catsAppended = [...cats];
+    catsAppended.push(name);
+    return catsAppended;
+}
+
+function prependCat(name){
+    const catsAppended = [...cats];
+    catsAppended.unshift(name);
+    return catsAppended;
+}
+
+function removeLastCat(){
+    const catsAppended = [...cats];
+    catsAppended.pop();
+    return catsAppended;
+}
+
+function removeFirstCat(){
+    const catsAppended = [...cats];
+    catsAppended.shift();
+    return catsAppended;
+}
